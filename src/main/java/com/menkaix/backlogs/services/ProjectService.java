68f5b7c8f86732fb6bac9d4df10396e76f1f8440
menkaix <mendrika.irina@gmail.com>
2024-04-26 16:13:24 +0300

    project management

---
+++ src/main/java/com/menkaix/backlogs/services/ProjectService.java
@@ -0,0 +1,58 @@
+package com.menkaix.backlogs.services;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.menkaix.backlogs.entities.Project;
+import com.menkaix.backlogs.repositories.ProjectRepisitory;
+import com.menkaix.backlogs.utilities.exceptions.DataConflictException;
+import com.menkaix.backlogs.utilities.exceptions.DataDefinitionException;
+
+@Service
+public class ProjectService {
+
+	private static Logger logger = LoggerFactory.getLogger(ProjectService.class);
+
+	@Autowired
+	private ProjectRepisitory repo;
+
+	public void safeCreateProject(Project projectCanditate) throws DataConflictException, DataDefinitionException {
+		List<Project> prjs = null;
+		if (projectCanditate.name != null) {
+			prjs = repo.findByName(projectCanditate.name);
+		} else if (projectCanditate.code != null) {
+			prjs = repo.findByCode(projectCanditate.code);
+			projectCanditate.name = projectCanditate.code;
+		} else {
+			throw new DataDefinitionException("Missing project name and code");
+		}
+
+		if (prjs.size() != 0) {
+			throw new DataConflictException("Project already exists");
+		} else {
+			repo.save(projectCanditate);
+		}
+	}
+
+	public Project findByNameOrCode(String in) {
+
+		List<Project> prjs = repo.findByName(in);
+		if (prjs.size() > 0) {
+			return prjs.get(0);
+		} else {
+			prjs = repo.findByCode(in);
+			if (prjs.size() > 0) {
+				return prjs.get(0);
+			} else {
+				logger.warn("no project found with " + in);
+				return null;
+			}
+		}
+
+	}
+
+}
---
+++ src/main/java/com/menkaix/backlogs/utilities/exceptions/DataConflictException.java
@@ -0,0 +1,14 @@
+package com.menkaix.backlogs.utilities.exceptions;
+
+public class DataConflictException extends Exception {
+
+	public DataConflictException(String string) {
+		super(string) ;
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1150307690503406434L;
+
+}
