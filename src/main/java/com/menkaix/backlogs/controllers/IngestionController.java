38b17395ac87cd5ce0175afeeb949b5e145fa033
menkaix <mendrika.irina@gmail.com>
2024-05-18 20:29:43 +0300

    batch ingest

--- src/main/java/com/menkaix/backlogs/controllers/IngestionController.java
+++ src/main/java/com/menkaix/backlogs/controllers/IngestionController.java
@@ -1,7 +1,13 @@
 package com.menkaix.backlogs.controllers;
 
+import java.util.List;
+import java.util.Map;
 import java.util.WeakHashMap;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,14 +20,43 @@ import com.menkaix.backlogs.services.ProjectService;
 
 @Controller
 public class IngestionController {
+
+	static Logger logger = LoggerFactory.getLogger(IngestionController.class) ;
 	
 	@Autowired
 	private ProjectService projectService ;
 	
 	@PostMapping("/ingest-story/{project}")
-	public ResponseEntity<String> ingestStrory(@RequestBody WeakHashMap<String, String> inputData, @PathVariable String project){
-		
-		String ans = projectService.ingestStory(project, inputData.get("data"));
+	public ResponseEntity<String> ingestStrory(@RequestBody Map<String, Object> inputData, @PathVariable String project){
+
+		String ans = "" ;
+
+		if(inputData.keySet().contains("data")){
+			ans = projectService.ingestStory(project, inputData.get("data").toString());
+		}
+		else if(inputData.keySet().contains("batch")){
+
+			logger.info("batch");
+
+			List<String> data = (List<String>)inputData.get("batch");
+
+			for (String elt: data) {
+
+				String stringElt = elt ;
+
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				}
+
+				logger.info("batch: "+stringElt);
+
+				ans += projectService.ingestStory(project, stringElt) +"\n";
+			}
+
+		}
+
 		
 		return new ResponseEntity<>(ans, HttpStatus.OK);
 	}
