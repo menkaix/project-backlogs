89afcdfec83e32e61d17422a454a8cb9917d98eb
mendrika.irina <mendrika.irina@gmail.com>
2024-08-19 11:12:28 +0000

    added setters and getters to the Project

--- src/main/java/com/menkaix/backlogs/entities/Project.java
+++ src/main/java/com/menkaix/backlogs/entities/Project.java
@@ -13,6 +13,28 @@ public class Project  extends AbstractEntity {
 
 	public String description ;
 
+	
+	public String getClientName() {
+		return clientName;
+	}
+
+	public void setClientName(String clientName) {
+
+		this.clientName = clientName;
+	}
+
+	public String getGroup() {
+		return group;
+	}
+
+	public void setGroup(String group) {
+		this.group = group;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
 	public Project() {
 		
 	}
