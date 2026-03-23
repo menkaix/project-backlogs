3ba5e51e0ee6de2b183e21b8a7d70568c92d24e0
menkaix <mendrika.irina@gmail.com>
2026-03-23 07:51:43 +0300

    retirer le cache backend

--- src/main/java/com/menkaix/backlogs/main/AccessingDataMongodbApplication.java
+++ src/main/java/com/menkaix/backlogs/main/AccessingDataMongodbApplication.java
@@ -3,12 +3,10 @@ package com.menkaix.backlogs.main;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@EnableCaching
 @EnableMongoAuditing
 @ComponentScan(basePackages = {
 		"com.menkaix.backlogs.services",
